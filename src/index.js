ae247c401fb65e53efb7f49164b4aaefb2aefa18
PauloRenato98 <paulo__159@hotmail.com>
2020-05-24 04:22:02 -0300

    Criando conexão com o banco de dados

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+require('dotenv').config();
 
 
-app.get('/', (req,res) => {
+app.get('/register', (req,res) => {
     res.send('hello world');
 })
+
+
+mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, () => {
+    console.log('Banco de Dados conectado');
+});
 
 app.listen(3000);
